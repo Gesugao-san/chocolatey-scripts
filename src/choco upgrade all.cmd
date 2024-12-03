72857f3fb1f3ba0ec42a3961bb708fcff47e1765
Gesugao-san <gesugao.san@gmail.com>
2024-12-03 03:15:06 +0300

    Add choco installation detection

--- src/choco upgrade all.cmd
+++ src/choco upgrade all.cmd
@@ -1,6 +1,15 @@
 CD C:\
 CLS
+@ECHO OFF
+IF "%path:chocolatey=%" EQU "%path%" (
+	ECHO Chocolatey not found
+	ECHO https://chocolatey.org/install
+	EXIT 1
+)
+@REM SET "choco=%ChocolateyInstall%\bin\choco.exe"
 @REM choco upgrade all --noop
+@ECHO ON
+
 choco outdated
 PAUSE
 CLS
