8d7c35b7924ceac6a1b4818f53fe2d418bbcd06f
Gesugao-san <gesugao.san@gmail.com>
2025-03-27 06:14:14 +0300

    Update choco upgrade all.cmd

--- src/choco upgrade all.cmd
+++ src/choco upgrade all.cmd
@@ -1,6 +1,7 @@
+@REM Moving to C drive to not advert user name.
 CD C:\
 CLS
-@ECHO OFF
+
 IF "%path:chocolatey=%" EQU "%path%" (
 	ECHO Chocolatey not found
 	ECHO https://chocolatey.org/install
@@ -10,9 +11,39 @@ IF "%path:chocolatey=%" EQU "%path%" (
 @REM choco upgrade all --noop
 @ECHO ON
 
+@REM List outdated chocolatey packages.
+@REM choco upgrade all --noop
 choco outdated
+
+@REM Confirm to proceed.
 PAUSE
 CLS
+
+@REM Find and apdate all chocolatey packages.
 choco upgrade all -y
+@REM Confirm to proceed.
 PAUSE
-refreshenv
+
+py -V&pip -V
+python.exe -m pip install --upgrade pip
+pip cache purge
+pip -V
+
+node -v&npm -v
+npm install -g npm@latest
+@REM npm cache verify
+npm cache clean -f
+npm -v
+
+yarn cache clean --all
+@REM pnpm store path
+@REM pnpm cache delete
+pnpm store prune
+
+@REM View what was cached.
+choco cache list
+@REM Remove cached data.
+choco cache remove
+
+@REM Unnessesary, except if PowerShell was in the updated list.
+RefreshEnv
