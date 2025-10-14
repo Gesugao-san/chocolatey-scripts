f9d0927184432a11a831357aefa5ec1e2b3b2554
Gesugao-san <gesugao.san@gmail.com>
2025-10-14 07:40:03 +0300

    Update scoop install minecraft redist.cmd

--- src/scoop install minecraft redist.cmd
+++ src/scoop install minecraft redist.cmd
@@ -3,6 +3,13 @@ scoop install temurin8-jre temurin16-jdk temurin17-jre temurin21-jre temurin-jre
 scoop bucket add unmojang https://github.com/unmojang/scoop-unmojang -y
 @REM scoop bucket add hero-persson https://github.com/hero-persson/scoop-unmojang
 scoop install fjordlauncher -y
+scoop bucket add java
+scoop install java/visualvm
+scoop bucket add games
+scoop install games/mcedit2
+scoop bucket add extras
+scoop install extras/notepad3
+@REM Manual install: https://www.ej-technologies.com/jprofiler/download → https://download.ej-technologies.com/jprofiler/jprofiler_windows-x64_15_0_3.exe
 cmd /c "scoop cache rm *"
 FOR %%i IN (cleanup checkup status) do cmd /c "scoop %%i *"
 PAUSE
