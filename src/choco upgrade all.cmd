643a9a8debe4d4c798d3cf7f095e1ccf6e8a61fe
Gesugao-san <gesugao.san@gmail.com>
2025-11-17 03:22:24 +0300

    Update choco upgrade all.cmd

--- src/choco upgrade all.cmd
+++ src/choco upgrade all.cmd
@@ -1,3 +1,4 @@
+@CHCP 65001 >nul
 @REM Moving to C drive to not advert user name.
 CD C:\
 CLS
@@ -8,6 +9,8 @@ CLS
 	@REM EXIT 1
 	@REM SEE ALSO https://gist.github.com/krayfaus/2a68fbc7386d3cdbcb45c577b1d4bae8
 	powershell -c "irm https://community.chocolatey.org/install.ps1|iex"
+  ECHO Restart me if I crashed or looped here, please. Sorry.
+  RefreshEnv
 )
 @REM SET "choco=%ChocolateyInstall%\bin\choco.exe"
 @REM choco upgrade all --noop
@@ -17,21 +20,27 @@ CLS
 @REM choco upgrade all --noop
 choco outdated
 
-@REM Confirm to proceed.
-PAUSE
-CLS
-
 @REM Find and apdate all chocolatey packages.
 choco upgrade all -y
 @REM Confirm to proceed.
-PAUSE
+@REM PAUSE
 
-@REM cmd /c "scoop status"
-@REM cmd /c "scoop update *"
-@REM cmd /c "scoop cache rm *"
-@REM cmd /c "scoop cleanup *"
-@REM cmd /c "scoop checkup"
-@REM cmd /c "scoop status"
+@WHERE scoop >nul 2>&1
+@IF ERRORLEVEL 1 (
+  @WHERE powershell >nul 2>&1 && (
+    echo Installing Scoop...
+    powershell -Command "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser"
+    powershell -Command "irm get.scoop.sh -outfile '%TEMP%\scoop-install.ps1'"
+    powershell -Command "& '%TEMP%\scoop-install.ps1' -RunAsAdmin"
+    @WHERE scoop >nul 2>&1
+    @IF ERRORLEVEL 1 (
+      ECHO Scoop installed! Restart me if I crashed or looped here, please. Sorry.
+      RefreshEnv
+    ) else (
+      echo Scoop installation failed!
+    )
+  )
+)
 @REM FOR %%i IN ("status" "update *" "cache rm *" "cleanup *" "checkup" "status") DO (cmd /c "scoop %%~i")
 FOR %%i IN (status update) do cmd /c "scoop %%i *"
 cmd /c "scoop cache rm *"
@@ -40,15 +49,16 @@ FOR %%i IN (cleanup checkup status) do cmd /c "scoop %%i *"
 @REM See https://redd.it/15oyh34
 yt-dlp -U
 
+@REM Python & pip updates
+@FOR %%i IN (python python2) DO @choco list --limit-output -e "%%i" | findstr /i "%%i" >nul || choco install "%%i" -y
 py -V&pip -V
-@REM python.exe -m pip install --upgrade pip
-choco install jq -y
-@REM See https://github.com/pypa/pip/issues/4551#issuecomment-2324945466
+python.exe -m pip install --upgrade pip
+@REM Installing jq (if not installed) for pip packages update
+@FOR %%i IN (jq) DO @choco list --limit-output -e "%%i" | findstr /i "%%i" >nul || choco install "%%i" -y
 FOR /F %%i IN ('pip list --format json ^| jq -r ".[].name"') DO pip install -U %%i
 pip cache purge
-pip -V
 
-choco install nodejs -y
+@FOR %%i IN (nodejs) DO @choco list --limit-output -e "%%i" | findstr /i "%%i" >nul || choco install "%%i" -y
 cmd /c "node -v&npm -v"
 @REM Upgrading to the latest version of npm
 cmd /c "npm install npm@latest -g"
@@ -58,12 +68,12 @@ cmd /c "npm update -g"
 cmd /c "npm cache clean -f"
 cmd /c "npm -v"
 
-choco install yarn -y
+@FOR %%i IN (yarn) DO @choco list --limit-output -e "%%i" | findstr /i "%%i" >nul || choco install "%%i" -y
 @REM npm install --global yarn
 cmd /c "yarn --version"
 cmd /c "yarn cache clean --all"
 
-choco install pnpm -y
+@FOR %%i IN (pnpm) DO @choco list --limit-output -e "%%i" | findstr /i "%%i" >nul || choco install "%%i" -y
 @REM scoop install nodejs nodejs-lts pnpm
 @REM pnpm store path
 @REM pnpm cache delete
