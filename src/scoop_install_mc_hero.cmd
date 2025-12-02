e6ba7affb4b77c3aa282baec87c7e177a149c3fc
Gesugao-san <gesugao.san@gmail.com>
2025-12-02 21:53:55 +0300

    Update scoop_install_mc_hero.cmd

--- src/scoop_install_mc_hero.cmd
+++ src/scoop_install_mc_hero.cmd
@@ -1,8 +1,8 @@
-scoop install main/7zip main/git -y
+scoop install main/7zip main/git
 FOR %%i IN (status update) do cmd /c "scoop %%i *"
-scoop install temurin8-jre temurin16-jdk temurin17-jre temurin21-jre temurin-jre glfw -y
+scoop install temurin8-jre temurin16-jdk temurin17-jre temurin21-jre temurin-jre glfw
 scoop bucket add hero-persson https://github.com/hero-persson/scoop-unmojang
-scoop install fjordlauncher -y
+scoop install fjordlauncher
 scoop bucket add java
 scoop install java/visualvm
 scoop bucket add games
