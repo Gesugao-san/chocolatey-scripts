f7cb5c546f9904d77106cff32e284d7eada0ca93
Gesugao-san <gesugao.san@gmail.com>
2025-12-02 21:47:01 +0300

    Update and rename scoop install minecraft redist.cmd to scoop_install_mc_unmojang.cmd

--- src/scoop_install_mc_unmojang.cmd
+++ src/scoop_install_mc_unmojang.cmd
@@ -2,7 +2,6 @@ scoop install main/7zip main/git -y
 FOR %%i IN (status update) do cmd /c "scoop %%i *"
 scoop install temurin8-jre temurin16-jdk temurin17-jre temurin21-jre temurin-jre glfw -y
 scoop bucket add unmojang https://github.com/unmojang/scoop-unmojang -y
-@REM scoop bucket add hero-persson https://github.com/hero-persson/scoop-unmojang
 scoop install fjordlauncher -y
 scoop bucket add java
 scoop install java/visualvm
