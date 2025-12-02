703ddcdf339adba907b4e155361dce13f002dfef
Gesugao-san <gesugao.san@gmail.com>
2025-12-02 21:47:42 +0300

    Create scoop_install_mc_hero.cmd

---
+++ src/scoop_install_mc_hero.cmd
@@ -0,0 +1,15 @@
+scoop install main/7zip main/git -y
+FOR %%i IN (status update) do cmd /c "scoop %%i *"
+scoop install temurin8-jre temurin16-jdk temurin17-jre temurin21-jre temurin-jre glfw -y
+scoop bucket add hero-persson https://github.com/hero-persson/scoop-unmojang
+scoop install fjordlauncher -y
+scoop bucket add java
+scoop install java/visualvm
+scoop bucket add games
+scoop install games/mcedit2
+scoop bucket add extras
+scoop install extras/notepad3
+@REM Manual install: https://www.ej-technologies.com/jprofiler/download → https://download.ej-technologies.com/jprofiler/jprofiler_windows-x64_15_0_3.exe
+cmd /c "scoop cache rm *"
+FOR %%i IN (cleanup checkup status) do cmd /c "scoop %%i *"
+PAUSE
