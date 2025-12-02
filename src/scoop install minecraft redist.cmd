570edfe657161808928f7e9f582d666848d4843f
Gesugao-san <gesugao.san@gmail.com>
2025-12-02 21:45:32 +0300

    Update scoop install minecraft redist.cmd

--- src/scoop install minecraft redist.cmd
+++ src/scoop install minecraft redist.cmd
@@ -1,3 +1,4 @@
+scoop install main/7zip main/git -y
 FOR %%i IN (status update) do cmd /c "scoop %%i *"
 scoop install temurin8-jre temurin16-jdk temurin17-jre temurin21-jre temurin-jre glfw -y
 scoop bucket add unmojang https://github.com/unmojang/scoop-unmojang -y
