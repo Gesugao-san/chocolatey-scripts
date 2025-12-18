ac13ade7c692ef60dad88f898a932ddba1aa937e
Gesugao-san <gesugao.san@gmail.com>
2025-12-18 23:55:02 +0300

    Update self-updating-test-v2.cmd

--- src/self-updating-test-v2.cmd
+++ src/self-updating-test-v2.cmd
@@ -1,4 +1,4 @@
-ECHO OFF
+@REM ECHO OFF
 SETLOCAL ENABLEDELAYEDEXPANSION
 
 SET "SCRIPT_URL=https://raw.githubusercontent.com/Gesugao-san/chocolatey-scripts/refs/heads/master/src/self-updating-test-v2.cmd"
@@ -8,8 +8,8 @@ SET "TEMP_FILE=%TEMP%\update_%random%.cmd"
 ECHO Checking for updates...
 ECHO Downloading: !SCRIPT_URL!
 
-:: 1. Download file to TEMP
-powershell -Command "Invoke-WebRequest -Uri '%SCRIPT_URL%' -OutFile '!TEMP_FILE!'" >nul 2>&1
+:: 1. Download file to TEMP directory
+powershell -Command "Invoke-WebRequest -Uri '!SCRIPT_URL!' -OutFile '!TEMP_FILE!'" >nul 2>&1
 
 IF NOT EXIST "!TEMP_FILE!" (
   ECHO Download failed
@@ -18,8 +18,8 @@ IF NOT EXIST "!TEMP_FILE!" (
 
 :: 2. Calculate SHA256 hashes of current and downloaded script
 ECHO Calculating hashes...
-FOR /f "delims=" %%A IN ('powershell -Command "Get-FileHash -Path '%SCRIPT_PATH%' -Algorithm SHA256 | %%{ $_.Hash }"') DO SET "CURRENT_HASH=%%A"
-FOR /f "delims=" %%A IN ('powershell -Command "Get-FileHash -Path '!TEMP_FILE!' -Algorithm SHA256 | %%{ $_.Hash }"') DO SET "NEW_HASH=%%A"
+FOR /f "delims=" %%A IN ('powershell -Command "Get-FileHash -Path '!SCRIPT_PATH!' -Algorithm SHA256 ^| %%{ $_.Hash }"') DO SET "CURRENT_HASH=%%A"
+FOR /f "delims=" %%A IN ('powershell -Command "Get-FileHash -Path '!TEMP_FILE!' -Algorithm SHA256 ^| %%{ $_.Hash }"') DO SET "NEW_HASH=%%A"
 
 :: 3. Compare hashes
 IF "!CURRENT_HASH!"=="!NEW_HASH!" (
@@ -30,11 +30,13 @@ IF "!CURRENT_HASH!"=="!NEW_HASH!" (
 
 :: 4. Replace and restart if hashes differ
 ECHO Update found! Replacing script...
-COPY "!TEMP_FILE!" "%SCRIPT_PATH%" /Y >nul
+COPY "!TEMP_FILE!" "!SCRIPT_PATH!" /Y >nul
 
 ECHO Restarting updated script...
 TIMEOUT /t 2 /nobreak >nul
-START "" "%SCRIPT_PATH%"
+
+:: 5. Restart with updated script using CMD /C
+CMD /C CALL "!SCRIPT_PATH!"
 DEL "!TEMP_FILE!" >nul 2>&1
 EXIT /b 0
 
