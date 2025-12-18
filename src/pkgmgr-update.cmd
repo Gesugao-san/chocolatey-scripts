089d2e636842ccc09ef807e62166f944510e247f
Gesugao-san <gesugao.san@gmail.com>
2025-12-19 00:10:53 +0300

    Update pkgmgr-update.cmd

--- src/pkgmgr-update.cmd
+++ src/pkgmgr-update.cmd
@@ -1,4 +1,3 @@
-SETLOCAL ENABLEDELAYEDEXPANSION
 @REM @CHCP 65001 >nul
 @REM @SET PYTHONIOENCODING=UTF-8
 @REM https://raw.githubusercontent.com/Gesugao-san/chocolatey-scripts/refs/heads/master/src/pkgmgr-update.cmd
@@ -8,14 +7,13 @@ CD C:\
 @ECHO ON
 CLS
 
-
-@REM === clink update ==========================================================
+@REM === clink update =========================================================
 FOR %%i IN (clink) DO WHERE %%i >nul 2>&1 && (
   cmd /c "%%i update"
   cmd /c "%%i inject"
 ) || (ECHO Tool %%i not found, skipping %%i update.)
 
-@REM === Internet connection check (multiple hosts) ============================
+@REM === Internet connection check (multiple hosts) =========================
 @REM ECHO Checking internet connection...
 SET "HOSTS=chocolatey.org scoop.sh github.com python.org nodejs.org yarnpkg.com pnpm.io"
 SET "CONNECTED=0"
@@ -37,51 +35,6 @@ IF !CONNECTED! EQU 0 (
 
 @REM ECHO Internet connection available, continuing...
 @REM ECHO.
-
-
-SET "SCRIPT_URL=https://raw.githubusercontent.com/Gesugao-san/chocolatey-scripts/refs/heads/master/src/pkgmgr-update.cmd"
-SET "SCRIPT_PATH=%~f0"
-SET "TEMP_FILE=%TEMP%\update_%random%.cmd"
-
-ECHO Checking for updates...
-ECHO Downloading: !SCRIPT_URL!
-
-:: 1. Download file to TEMP directory
-powershell -Command "Invoke-WebRequest -Uri '!SCRIPT_URL!' -OutFile '!TEMP_FILE!'" >nul 2>&1
-
-IF NOT EXIST "!TEMP_FILE!" (
-  ECHO Download failed
-  GOTO :main
-)
-
-:: 2. Calculate SHA256 hashes of current and downloaded script
-ECHO Calculating hashes...
-FOR /f "delims=" %%A IN ('powershell -Command "Get-FileHash -Path '!SCRIPT_PATH!' -Algorithm SHA256 ^| %%{ $_.Hash }"') DO SET "CURRENT_HASH=%%A"
-FOR /f "delims=" %%A IN ('powershell -Command "Get-FileHash -Path '!TEMP_FILE!' -Algorithm SHA256 ^| %%{ $_.Hash }"') DO SET "NEW_HASH=%%A"
-
-:: 3. Compare hashes
-IF "!CURRENT_HASH!"=="!NEW_HASH!" (
-  ECHO No updates available
-  DEL "!TEMP_FILE!" >nul 2>&1
-  GOTO :main
-)
-
-:: 4. Replace and restart if hashes differ
-ECHO Update found! Replacing script...
-COPY "!TEMP_FILE!" "!SCRIPT_PATH!" /Y >nul
-
-ECHO Restarting updated script...
-TIMEOUT /t 2 /nobreak >nul
-
-:: 5. Restart with updated script using CMD /C
-CMD /C CALL "!SCRIPT_PATH!"
-DEL "!TEMP_FILE!" >nul 2>&1
-EXIT /b 0
-
-:main
-:: Main script logic
-ECHO Performing main tasks...
-
 
 @REM === Chocolatey install ====================================================
 @IF "%path:chocolatey=%" EQU "%path%" (
