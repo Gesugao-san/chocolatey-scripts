e0d4215fc07d8177818da4907672f1e88dd30fd4
Gesugao-san <gesugao.san@gmail.com>
2026-01-13 00:05:43 +0300

    Update scoop_install_mc_hero_legacy.ps1

--- src/scoop_install_mc_hero_legacy.ps1
+++ src/scoop_install_mc_hero_legacy.ps1
@@ -3,6 +3,7 @@ scoop update
 scoop install main/7zip main/git
 scoop status 
 scoop update
+scoop bucket add java
 scoop install temurin8-jre temurin16-jdk temurin17-jre temurin21-jre temurin-jre glfw
 scoop bucket add hero-persson https://github.com/hero-persson/scoop-unmojang
 scoop install hero-persson/fjordlauncher
