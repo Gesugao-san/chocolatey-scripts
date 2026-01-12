85c889d2b1d336c1b444c70412f53c84cd7cc694
Gesugao-san <gesugao.san@gmail.com>
2026-01-13 00:05:34 +0300

    Update scoop_install_mc_hero.cmd

--- src/scoop_install_mc_hero.cmd
+++ src/scoop_install_mc_hero.cmd
@@ -1,6 +1,7 @@
 @CD C:\
 scoop install main/7zip main/git
 FOR %%i IN (status update) do cmd /c "scoop %%i *"
+scoop bucket add java
 scoop install temurin8-jre temurin16-jdk temurin17-jre temurin21-jre temurin-jre glfw
 scoop bucket add hero-persson https://github.com/hero-persson/scoop-unmojang
 scoop install hero-persson/fjordlauncher
